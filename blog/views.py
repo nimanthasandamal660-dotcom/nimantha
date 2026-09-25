5450fa16434108d84bc60172c0825ebc7b7b816f
nimantha <nimanthasandamal660@gmail.com>
2026-09-25 10:03:51 +0900

    Add base template, static CSS, and contact page

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "blog/home.html")
+
+def about(request):
+    return render(request, "blog/about.html", {"team": "DjangoBlog Team"})
+def home(request):
+    return render(request, "blog/home.html")
+def about(request):
+    return render(request, "blog/about.html", {"team": "DjangoBlog Team"})
+def contact(request):
+    return render(request, "blog/contact.html")
