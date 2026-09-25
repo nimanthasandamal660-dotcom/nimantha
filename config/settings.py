ff2212d3e4e93ffa582824a357d2b61c7f26038b
nimantha <nimanthasandamal660@gmail.com>
2026-09-25 10:29:30 +0900

    Set up admin interface and Debug Toolbar; seed sample posts

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,4 @@ STATICFILES_DIRS = [BASE_DIR / "blog" / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+INTERNAL_IPS = ["127.0.0.1"]
